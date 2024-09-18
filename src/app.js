787177589d901f8e8f5c79919847660f701c2b63
Parit Bhardwaj <paritbhardwaj@outlook.com>
2024-09-18 13:47:15 +0530

    feat: cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://albearoti-client.vercel.app"],
     credentials: true,
   })
 );
